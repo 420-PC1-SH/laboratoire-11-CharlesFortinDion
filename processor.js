3dd3370543835f04609a3c30e3f47622227f9f76
CharlesFortinDion <201398083@cegepsherbrooke.qc.ca>
2023-06-14 14:57:25 -0400

    requis 4 passed

---
+++ processor.js
@@ -0,0 +1,13 @@
+function processor(transmission) {
+    if (transmission.indexOf("::") < 0) {
+        // Data is invalid
+        return -1;
+    }
+    let parts = transmission.split("::");
+    return {
+        id: Number(parts[0])
+    };
+}
+
+
+module.exports = processor;
---
+++ processor.test.js
@@ -0,0 +1,24 @@
+const processor = require('./processor.js');
+
+describe("transmission processor", function () {
+
+    test("takes a string and returns an object", function () {
+        let result = processor("9701::<489584872710>");
+        expect(typeof result).toEqual("object");
+    });
+
+    test("returns -1 if '::' not found", function () {
+        let result = processor("9701<489584872710>");
+        expect(result).toEqual(-1);
+    });
+
+    test("returns id in object", function () {
+        let result = processor("9701::<489584872710>");
+        expect(result.id).not.toEqual(undefined);
+    });
+
+    test("id property is a number", function () {
+        let result = processor("9701::<489584872710>");
+        expect(result.id).toEqual(9701);
+    })
+});
