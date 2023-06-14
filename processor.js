a18d0d5ebd91df29d4cf064ddd005da54aa8eb30
CharlesFortinDion <201398083@cegepsherbrooke.qc.ca>
2023-06-14 15:29:36 -0400

    requis 6 pass

--- processor.js
+++ processor.js
@@ -5,9 +5,14 @@ function processor(transmission) {
     }
     let parts = transmission.split("::");
     let rawData = parts[1];
+    if (rawData[0] !== '<' || rawData[rawData.length - 1] !== '>') {
+        rawData = -1;
+    } else {
+        rawData = rawData.replace(/[<>]/g, "");
+    }
     return {
         id: Number(parts[0]),
-        rawData: rawData
+        rawData: Number(rawData)
     };
 }
 
